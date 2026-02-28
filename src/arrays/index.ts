9796bbf68b23eadfaaedec2dcdb82b7943b97a5b
Jess Graham <jess@jessgraham.dev>
2026-02-28 17:33:52 -0500

    Move palindrome test question to two-pointers

--- src/arrays/index.ts
+++ src/arrays/index.ts
@@ -97,39 +97,3 @@ function containsDuplicate(nums: number[]): boolean {
 
   return false;
 }
-
-function isPalindrome(s: string): boolean {
-  //  Helper functions
-  const isAlphanumeric = (char: string): boolean => /[a-z0-9]/.test(char.toLowerCase());
-
-  let leftPtr = 0;
-  let rightPtr = s.length - 1;
-
-  // Complexity O(n)
-  while (leftPtr < rightPtr) {
-    const left = s[leftPtr]!.toLowerCase();
-    const right = s[rightPtr]!.toLowerCase();
-
-    const leftIsValid = isAlphanumeric(left);
-    const rightIsValid = isAlphanumeric(right);
-
-    if (!leftIsValid) {
-      leftPtr++;
-      continue;
-    }
-
-    if (!rightIsValid) {
-      rightPtr--;
-      continue;
-    }
-
-    if (left === right) {
-      leftPtr++;
-      rightPtr--;
-    } else {
-      return false;
-    }
-  }
-
-  return true;
-}
--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,8 @@
-function isPalindrome(s: string): boolean {
-  //  Helper functions
-  const isAlphanumeric = (char: string): boolean => /[a-z0-9]/.test(char.toLowerCase());
-
-  let leftPtr = 0;
-  let rightPtr = s.length - 1;
-
-  // Complexity O(n)
-  while (leftPtr < rightPtr) {
-    const left = s[leftPtr]!.toLowerCase();
-    const right = s[rightPtr]!.toLowerCase();
-
-    const leftIsValid = isAlphanumeric(left);
-    const rightIsValid = isAlphanumeric(right);
-
-    if (!leftIsValid) {
-      leftPtr++;
-      continue;
-    }
-
-    if (!rightIsValid) {
-      rightPtr--;
-      continue;
-    }
-
-    if (left === right) {
-      leftPtr++;
-      rightPtr--;
-    } else {
-      return false;
-    }
-  }
-
-  return true;
-}
-
 const main = () => {
   console.log('🚀 Starting coding practice...');
   console.log('================================');
 
-  console.log(isPalindrome('A man, a plan, a canal: Panama'));
+  // console.log(isPalindrome('A man, a plan, a canal: Panama'));
 
   console.log('================================');
 };
